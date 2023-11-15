051cacf29376f11c2fd7bdba34777ca1869baacc
pabcueper <pabcueper@alum.us.es>
2023-11-15 15:11:36 +0100

    feat: RegistroUsuarios Cambio en la idea de como realizarlo, esta manera de funcionar corre los tests. Falta implementar resto de categorías.

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -1,16 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.status import (
         HTTP_201_CREATED,
-        HTTP_200_OK,
         HTTP_400_BAD_REQUEST,
-        HTTP_401_UNAUTHORIZED,
-        HTTP_404_NOT_FOUND
+        HTTP_401_UNAUTHORIZED
 )
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
@@ -24,43 +21,37 @@ class GetUserView(APIView):
         tk = get_object_or_404(Token, key=key)
         return Response(UserSerializer(tk.user, many=False).data)
 
+#Esto seguramente se tendrá que eliminar cuando se haga el logout bien
 class LogoutView(APIView):
     def post(self, request):
+        key = request.data.get('token', '')
         try:
-            key = request.data.get('token', '')
-            Token.objects.filter(key=key).delete()
-            return Response('User logout succesfully', status=HTTP_200_OK)
+            tk = Token.objects.get(key=key)
+            tk.delete()
         except ObjectDoesNotExist:
-            return Response('', status=HTTP_404_NOT_FOUND)
+            pass
+
+        return Response({'authentication/login'})
 
 
-class RegisterForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = (
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'password'
-        )
-        labels = {
-            'username':('Username'),
-            'first_name':('First Name'),
-            'last_name':('Last Name'),
-            'email':('Email'),
-            'password':('Password')
-        }
-        #Revisar nombre de usuario(repeticiones, tamaño etc...), mirar formato y tamaño de email y comprobar tamaño de los campos de nombre y apellido.
-
-class RegisterView(CreateView):
-    
+      
+class RegisterView(APIView):
     def post(self, request):
-        form = RegisterForm(request.data)
-        if form.is_valid():
-            user = form.save()
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({'user_pk': user.pk, 'token': token.key}, status=HTTP_201_CREATED)
-        else:
-            return Response(form.errors, status=HTTP_400_BAD_REQUEST)
-#Hay que añadir vista de cerrar sesión, el main donde se lanza todo y una clase para la vista de login.
+        key = request.data.get('token', '')
+        tk = get_object_or_404(Token, key=key)
+        if not tk.user.is_superuser:
+            return Response({}, status=HTTP_401_UNAUTHORIZED)
+
+        username = request.data.get('username', '')
+        pwd = request.data.get('password', '')
+        if not username or not pwd:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User(username=username)
+            user.set_password(pwd)
+            user.save()
+            token,  _= Token.objects.get_or_create(user=user)
+        except IntegrityError:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+        return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
