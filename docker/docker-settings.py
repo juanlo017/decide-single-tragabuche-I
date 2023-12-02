ee3e575245fa0b0606036ffa55e19f9286338bd0
juanlo <juanquiroslopez@gmail.com>
2023-12-02 18:16:15 +0100

    fix

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
@@ -15,7 +15,7 @@ STATIC_ROOT = '/app/static/'
 MEDIA_ROOT = '/app/static/media/'
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://10.5.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://10.5.0.1:8000', 'http://localhost:8000']
 
 # Modules in use, commented modules that you won't use
 MODULES = [
